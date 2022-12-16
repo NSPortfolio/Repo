9b23bc019f69a9da1159fdedb4f36dd2cd6b9617
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-12-16 15:03:06 -0600

    Update test_SpindlerBattery.py

--- UnitTest/BatteryTests/test_SpindlerBattery.py
+++ UnitTest/BatteryTests/test_SpindlerBattery.py
@@ -1,11 +1,16 @@
 from Battery.SpindlerBattery import SpindlerBattery
 import unittest
+from datetime import datetime
 
-class TestNubbinBattery(unittest.TestCase):
+class SpindlerBattery(unittest.TestCase):
     def test_needs_service(self):
-        result = SpindlerBattery('2022-12-16', '2018-12-16')
+        current_date = date.fromisoformat("2022-12-16")
+        last_service_date = date.fromisoformat("2017-12-16")
+        result = SpindlerBattery(current_date, last_service_date)
         self.assertTrue(result)
-        result = SpindlerBattery('2022-12-16', '2021-12-16')
+        current_date = date.fromisoformat("2022-12-16")
+        last_service_date = date.fromisoformat("2020-12-16")
+        result = SpindlerBattery(current_date, last_service_date)
         self.assertFalse(result)
 
 if __name__ -- '__main__':
