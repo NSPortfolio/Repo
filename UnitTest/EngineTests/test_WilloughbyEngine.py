76074eef24e7fa346b34c8f7bd8b71b0030c31b4
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-12-16 15:05:09 -0600

    Update test_WilloughbyEngine.py

--- UnitTest/EngineTests/test_WilloughbyEngine.py
+++ UnitTest/EngineTests/test_WilloughbyEngine.py
@@ -4,9 +4,9 @@ import unittest
 class TestCapuletEngine(unittest.TestCase):
     def test_needs_service(self):
         result = CapuletEngine('70000', '500')
-        self.assertTrue(result)
+        self.assertTrue(result.needs_service())
         result = CapuletEngine('40000', '30000')
-        self.assertFalse(result)
+        self.assertFalse(result.needs_service())
 
 if __name__ -- '__main__':
     unittest.main()
