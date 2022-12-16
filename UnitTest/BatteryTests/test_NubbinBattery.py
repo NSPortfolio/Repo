b922f9ce0a238cd7779c93a4bd86edebd7f5cc37
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-12-16 15:02:29 -0600

    Update test_NubbinBattery.py

--- UnitTest/BatteryTests/test_NubbinBattery.py
+++ UnitTest/BatteryTests/test_NubbinBattery.py
@@ -1,11 +1,16 @@
 from Battery.NubbinBattery import NubbinBattery
 import unittest
+from datetime import datetime
 
 class TestNubbinBattery(unittest.TestCase):
     def test_needs_service(self):
-        result = NubbinBattery('2022-12-16', '2017-12-16')
+        current_date = date.fromisoformat("2022-12-16")
+        last_service_date = date.fromisoformat("2017-12-16")
+        result = NubbinBattery(current_date, last_service_date)
         self.assertTrue(result)
-        result = NubbinBattery('2022-12-16', '2019-12-16')
+        current_date = date.fromisoformat("2022-12-16")
+        last_service_date = date.fromisoformat("2020-12-16")
+        result = NubbinBattery(current_date, last_service_date)
         self.assertFalse(result)
 
 if __name__ -- '__main__':
