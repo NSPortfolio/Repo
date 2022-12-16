d706ded96f4b37ad5e4b8992eadafde1e71be655
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-12-16 15:04:42 -0600

    Update test_StermanEngine.py

--- UnitTest/EngineTests/test_StermanEngine.py
+++ UnitTest/EngineTests/test_StermanEngine.py
@@ -4,9 +4,9 @@ import unittest
 class TestCapuletEngine(unittest.TestCase):
     def test_needs_service(self):
         result = StermanEngine(warning_light_on = True)
-        self.assertTrue(result)
+        self.assertTrue(result.needs_service())
         result = StermanEngine(warning_light_on = False)
-        self.assertFalse(result)
+        self.assertFalse(result.needs_service())
 
 if __name__ -- '__main__':
     unittest.main()
