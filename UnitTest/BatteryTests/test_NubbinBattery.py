75043cc908ffcb4cd533e895a6094f31c57e5310
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-12-16 15:04:07 -0600

    Update test_NubbinBattery.py

--- UnitTest/BatteryTests/test_NubbinBattery.py
+++ UnitTest/BatteryTests/test_NubbinBattery.py
@@ -7,11 +7,11 @@ class TestNubbinBattery(unittest.TestCase):
         current_date = date.fromisoformat("2022-12-16")
         last_service_date = date.fromisoformat("2017-12-16")
         result = NubbinBattery(current_date, last_service_date)
-        self.assertTrue(result)
+        self.assertTrue(result.needs_service())
         current_date = date.fromisoformat("2022-12-16")
         last_service_date = date.fromisoformat("2020-12-16")
         result = NubbinBattery(current_date, last_service_date)
-        self.assertFalse(result)
+        self.assertFalse(result.needs_service())
 
 if __name__ -- '__main__':
     unittest.main()
